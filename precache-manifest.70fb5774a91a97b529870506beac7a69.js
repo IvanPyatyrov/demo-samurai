f94ab133829238d23b9da6834a16fa4bd797fc1c
IvanPyatyrov <36262990+IvanPyatyrov@users.noreply.github.com>
2020-07-31 15:34:15 +1000

    3

---
+++ precache-manifest.70fb5774a91a97b529870506beac7a69.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c93acc032910474e1485a10571644f",
+    "url": "/demo-samurai/index.html"
+  },
+  {
+    "revision": "4673752991906235dc6e",
+    "url": "/demo-samurai/static/css/3.008f7de5.chunk.css"
+  },
+  {
+    "revision": "a296428ebf81d72c4fd2",
+    "url": "/demo-samurai/static/css/4.71995090.chunk.css"
+  },
+  {
+    "revision": "3976b578db900d4b7ff0",
+    "url": "/demo-samurai/static/css/main.7a05bc0c.chunk.css"
+  },
+  {
+    "revision": "0e69f38320e9a1b8f309",
+    "url": "/demo-samurai/static/js/2.7d89d280.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/demo-samurai/static/js/2.7d89d280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4673752991906235dc6e",
+    "url": "/demo-samurai/static/js/3.1d71f8b8.chunk.js"
+  },
+  {
+    "revision": "a296428ebf81d72c4fd2",
+    "url": "/demo-samurai/static/js/4.8963ebb0.chunk.js"
+  },
+  {
+    "revision": "3976b578db900d4b7ff0",
+    "url": "/demo-samurai/static/js/main.541416db.chunk.js"
+  },
+  {
+    "revision": "a31c79fcb11bebea4e22",
+    "url": "/demo-samurai/static/js/runtime-main.be89cd30.js"
+  },
+  {
+    "revision": "f4d7875d836558dc64082fcc6d7d0b6c",
+    "url": "/demo-samurai/static/media/Preloader.f4d7875d.svg"
+  },
+  {
+    "revision": "385e14270581eab76648f2e992709713",
+    "url": "/demo-samurai/static/media/user.385e1427.png"
+  }
+]);
